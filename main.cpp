bf499cdf77b95279cb1024145ef10054936fd8b5
ahmadfahrudin98 <43611815+ahmadfahrudin98@users.noreply.github.com>
2019-11-03 20:57:50 +0700

    update tampilan
    penambahan ground mobil dan tata letak pemindahan lokasi

--- main.cpp
+++ main.cpp
@@ -626,3 +626,477 @@ glEnd();
 glColor3f(0.0,0.0,1.0);
 
 }
+
+void drawhouse()
+{
+ glBegin(GL_LINE_LOOP);
+
+    glVertex3f(-2.6,-.84,2.5);//m
+    glVertex3f(-2.6,0.84,2.5);//n
+    glVertex3f(-3.04,0.84,2.8);//o
+    glVertex3f(0,1.95,2.8);//p
+    glVertex3f(3.04,0.84,2.8);//w
+    glVertex3f(2.6,0.84,2.5);//q
+    glVertex3f(2.6,-0.84,2.5);//r
+       glVertex3f(1.59,-0.84,2.5);//s
+    glVertex3f(1.59,0.16,2.5);//t
+    glVertex3f(-1.59,0.16,2.5);//u
+    glVertex3f(-1.59,-0.84,2.5);//v
+    glEnd();
+    glBegin(GL_LINES);
+    glVertex3f(1.59,-0.84,2.5);//s
+       glVertex3f(-1.59,-0.84,2.5);//v
+    glEnd();
+
+    glBegin(GL_LINE_LOOP);
+       glVertex3f(-2.6,-.84,-2.5);//m'
+    glVertex3f(-2.6,0.84,-2.5);//n'
+    glVertex3f(-3.04,0.84,-2.8);//o'
+    glVertex3f(0,1.95,-2.8);//p'
+    glVertex3f(3.04,0.84,-2.8);//w'
+    glVertex3f(2.6,0.84,-2.5);//q'
+    glVertex3f(2.6,-0.84,-2.5);//r'
+       glVertex3f(1.59,-0.84,-2.5);//s'
+    glVertex3f(1.59,0.16,-2.5);//t'
+    glVertex3f(-1.59,0.16,-2.5);//u'
+    glVertex3f(-1.59,-0.84,-2.5);//v'
+       glEnd();
+
+       glBegin(GL_LINES);
+       glVertex3f(-2.6,-.84,2.5);//m
+     glVertex3f(-2.6,-.84,-2.5);//m'
+        glVertex3f(-2.6,0.84,2.5);//n
+  glVertex3f(-2.6,0.84,-2.5);//n'
+         glVertex3f(-3.04,0.84,2.8);//o
+   glVertex3f(-3.04,0.84,-2.8);//o'
+   glVertex3f(0,1.95,2.8);//p
+    glVertex3f(0,1.95,-2.8);//p'
+
+    glVertex3f(3.04,0.84,2.8);//w
+       glVertex3f(3.04,0.84,-2.8);//w'
+    glVertex3f(2.6,0.84,2.5);//q
+    glVertex3f(2.6,0.84,-2.5);//q'
+    glVertex3f(2.6,-0.84,2.5);//r
+    glVertex3f(2.6,-0.84,-2.5);//r'
+       glVertex3f(1.59,-0.84,2.5);//s
+       glVertex3f(1.59,-0.84,-2.5);//s'
+
+    glVertex3f(-1.59,-0.84,2.5);//v
+       glVertex3f(-1.59,-0.84,-2.5);//v'
+
+
+    glEnd();
+    glColor3ub(255,185,1);//*************
+
+    glBegin(GL_QUADS);
+        glVertex3f(-2.6,-.84,2.5);//m
+  glVertex3f(-2.6,0.16,2.5);//uu
+  glVertex3f(-1.59,0.16,2.5);//u
+  glVertex3f(-1.59,-0.84,2.5);//v
+
+       glVertex3f(-2.6,0.16,2.5);//uu
+       glVertex3f(-2.6,0.84,2.5);//n
+     glVertex3f(2.6,0.84,2.5);//q
+  glVertex3f(2.6,0.16,2.5);//tt
+
+
+     glVertex3f(1.59,-0.84,2.5);//s
+    glVertex3f(1.59,0.16,2.5);//t
+        glVertex3f(2.6,0.16,2.5);//tt
+   glVertex3f(2.6,-0.84,2.5);//r
+
+
+        glVertex3f(-2.6,-.84,-2.5);//m'
+  glVertex3f(-2.6,0.16,-2.5);//uu'
+  glVertex3f(-1.59,0.16,-2.5);//u'
+  glVertex3f(-1.59,-0.84,-2.5);//v'
+
+       glVertex3f(-2.6,0.16,-2.5);//uu'
+       glVertex3f(-2.6,0.84,-2.5);//n'
+     glVertex3f(2.6,0.84,-2.5);//q'
+  glVertex3f(2.6,0.16,-2.5);//tt'
+
+
+     glVertex3f(1.59,-0.84,-2.5);//s'
+    glVertex3f(1.59,0.16,-2.5);//t'
+        glVertex3f(2.6,0.16,-2.5);//tt'
+   glVertex3f(2.6,-0.84,-2.5);//r'
+
+   glVertex3f(-2.6,-.84,2.5);//m
+     glVertex3f(-2.6,-.84,-2.5);//m'
+        glVertex3f(-2.6,0.84,-2.5);//n'
+  glVertex3f(-2.6,0.84,2.5);//n
+
+  glVertex3f(2.6,0.84,2.5);//q
+    glVertex3f(2.6,0.84,-2.5);//q'
+    glVertex3f(2.6,-0.84,-2.5);//r'
+    glVertex3f(2.6,-0.84,2.5);//r
+
+    glEnd();
+
+
+
+
+
+   glBegin(GL_TRIANGLES);
+glVertex3f(0,1.95,2.5);//p
+      glVertex3f(3.04,0.84,2.5);//w
+         glVertex3f(-3.04,0.84,2.5);//o
+   glVertex3f(0,1.95,-2.5);//p'
+      glVertex3f(3.04,0.84,-2.5);//w'
+         glVertex3f(-3.04,0.84,-2.5);//o'
+
+glEnd();
+  glColor3ub(255,102,0);//***********top color
+
+    glBegin(GL_QUADS);
+
+     glVertex3f(0,1.95,2.8);//p
+    glVertex3f(0,1.95,-2.8);//p'
+       glVertex3f(3.04,0.84,-2.8);//w'
+       glVertex3f(3.04,0.84,2.8);//w
+
+     glVertex3f(-3.04,0.84,2.8);//o
+   glVertex3f(-3.04,0.84,-2.8);//o'
+   glVertex3f(0,1.95,-2.8);//p'
+    glVertex3f(0,1.95,2.8);//p
+
+          glEnd();
+
+     glColor3ub(116,18,0);//*******base color
+
+    glBegin(GL_QUADS);
+    glVertex3f(-2.6,-.84,2.5);//m
+          glVertex3f(2.6,-0.84,2.5);//r
+    glVertex3f(2.6,-0.84,-2.5);//r'
+    glVertex3f(-2.6,-.84,-2.5);//m'
+    glEnd();
+
+
+
+}
+
+
+
+
+GLuint createDL() {
+ GLuint carrDL;
+
+ // Create the id for the list
+ carrDL = glGenLists(1);
+
+ // start list
+ glNewList(carrDL,GL_COMPILE);
+
+ // call the function that contains the rendering commands
+  drawcarr();
+
+ // endList
+ glEndList();
+
+ return(carrDL);
+}
+GLuint createDL2()//******************
+{
+ GLuint houseDL;
+
+ // Create the id for the list
+ houseDL = glGenLists(1);
+
+ // start list
+ glNewList(houseDL,GL_COMPILE);
+
+ // call the function that contains the rendering commands
+  drawhouse();
+
+ // endList
+ glEndList();
+
+ return(houseDL);
+}//**************
+
+void initScene()
+{
+
+ glEnable(GL_DEPTH_TEST);
+ carr_display_list = createDL();
+ house_display_list= createDL2();//***********
+
+}
+
+
+
+
+
+void renderScene(void)
+{
+ int i,j;
+ glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+ glClearColor(.7,0.85,1.0,1.0);
+
+// Draw ground
+
+ glColor3f(0.25f, 0.25f, 0.25f);
+ glBegin(GL_QUADS);
+  glVertex3f(-100.0f, 0.0f, -100.0f);
+  glVertex3f(-100.0f, 0.0f,  100.0f);
+  glVertex3f( 100.0f, 0.0f,  100.0f);
+  glVertex3f( 100.0f, 0.0f, -100.0f);
+ glEnd();
+
+// Draw 36 car
+
+ for( i = 2; i < 4; i++)//kolom
+  for( j=2; j < 4; j++) //baris
+  {
+   glPushMatrix();
+   glTranslatef((i)*10.0,0,(j) * 10.0);
+   glColor3ub(a[i],b[j],c[i]);
+   glCallList(carr_display_list);
+   glPopMatrix();
+  }
+ if(housevisible)
+ {
+ glPushMatrix();
+   glScalef(2.0,2.0,2.0);
+   glTranslatef(0.0,.85,-20.0);
+   glCallList(house_display_list);
+           glTranslatef(10.0,0.0,0.0);
+     glCallList(house_display_list);
+          glTranslatef(-20.0,0.0,0.0);
+     glCallList(house_display_list);
+
+   glRotatef(90,0.0,1.0,0.0);
+         glTranslatef(-10.0,0.0,-10.0);
+   glCallList(house_display_list);
+
+   glTranslatef(-10.0,0.0,0.0);
+   glCallList(house_display_list);
+   glTranslatef(-10.0,0.0,0.0);
+   glCallList(house_display_list);
+   glPopMatrix();
+
+  glPushMatrix();
+         glTranslatef(10.0,3.4,-80.0);
+         glScalef(4.0,4.0,4.0);
+   glCallList(house_display_list);
+   glTranslatef(-10.0,0.0,0.0);
+   glCallList(house_display_list);
+  glPopMatrix();
+
+         glPushMatrix();
+            glRotatef(90,0.0,1.0,0.0);
+    glScalef(2.0,2.0,2.0);
+    glTranslatef(0.0,0.85,15.0);
+             glCallList(house_display_list);
+    glTranslatef(10.0,0.,0.0);
+             glCallList(house_display_list);
+                  glTranslatef(-20.0,0.,0.0);
+             glCallList(house_display_list);
+         glPopMatrix();
+
+ }
+ if(fxincr!=0)
+ theta1=(atan(fzincr/fxincr)*180)/3.141;
+ else if(fzincr>0)
+  theta1=-90.0;
+ else theta1=90.0;
+
+ if(fxincr>0&&fzincr<0)
+ {
+  theta1=-theta1;
+ }
+ else if(fxincr<0&&fzincr<0)
+ {
+  theta1=180-theta1;
+ }
+ else if(fxincr<0&&fzincr>0)
+ {
+  theta1=-180-theta1;
+ }else if(fxincr>0&&fzincr>0)
+ {
+ theta1=-theta1;
+ }
+ //else theta1=90;
+ //glLoadIdentity();
+    glPushMatrix();
+    glTranslatef(fx,0,fz);
+    glRotatef(theta1,0,1,0);
+    glColor3f(0.8,0.8,0);
+    glCallList(carr_display_list);
+    glPopMatrix();
+
+    glutSwapBuffers();
+
+}
+
+void orientMe(float ang)
+{
+    lx = sin(ang);
+ lz = -cos(ang);
+ glLoadIdentity();
+ gluLookAt(x, y, z,
+        x + lx,y + ly,z + lz,
+     0.0f,1.0f,0.0f);
+
+}
+
+
+void moveMeFlat(int i)
+{
+if(xxxx==1)
+  y=y+i*(lz)*0.1;//*********
+
+if(yyyy==1)
+{
+ x=x+i*(lz)*.1;
+}
+else
+{
+ z = z + i*(lz)*0.5;
+           x = x + i*(lx)*0.5;}
+glLoadIdentity();
+ gluLookAt(x, y, z,
+        x + lx,y + ly,z + lz,
+     0.0f,1.0f,0.0f);
+
+}
+
+void processNormalKeys(unsigned char key, int x, int y)
+{
+glLoadIdentity();
+
+
+ if (key == 'q')
+  exit(0);
+         if(key=='t')
+          gluLookAt(1,190,50,0,0 ,-10,0.0,1.0,.0);
+  if(key=='a') moveMeFlat(4);xxxx=1,yyyy=0;
+     if(key=='s') moveMeFlat(-4);xxxx=1,yyyy=0;
+       if(key=='w') moveMeFlat(4);yyyy=1;xxxx=0;
+       if(key=='d') moveMeFlat(-4);yyyy=1;xxxx=0;
+
+
+
+
+}
+
+
+void inputKey(int key, int x, int y)
+{
+
+ switch (key)
+ {
+  case GLUT_KEY_LEFT : angle -= 0.05f;orientMe(angle);break;
+  case GLUT_KEY_RIGHT : angle +=0.05f;orientMe(angle);break;
+  case GLUT_KEY_UP : moveMeFlat(2);xxxx=0,yyyy=0;break;
+  case GLUT_KEY_DOWN : moveMeFlat(-2);xxxx=0,yyyy=0;break;
+
+ }
+}
+void movecar(int key, int x, int y)
+{
+
+ switch (key)
+ {
+  case GLUT_KEY_LEFT :temp=fxincr;
+       fxincr=fxincr*cos(theta)+fzincr*sin(theta);
+       fzincr=-temp*sin(theta)+fzincr*cos(theta);
+       fx+=fxincr;
+       fz+=fzincr;
+
+       break;
+  case GLUT_KEY_RIGHT :temp=fxincr;
+       fxincr=fxincr*cos(-theta)+fzincr*sin(-theta);
+       fzincr=-temp*sin(-theta)+fzincr*cos(-theta);
+       fx+=fxincr;
+       fz+=fzincr;
+
+       break;
+  case GLUT_KEY_UP :fx+=fxincr;
+        fz+=fzincr;break;
+  case GLUT_KEY_DOWN :fx-=fxincr;
+                  fz-=fzincr; break;
+
+ }
+ glutPostRedisplay();
+}
+
+// Reset flags as appropriate in response to menu selections
+void ProcessMenu(int value)
+{
+    glutPostRedisplay();
+}
+
+void ProcessMenu1(int value)
+{
+ switch(value)
+ {
+ case 1:if(housevisible==0)
+          housevisible=1;
+   else
+    housevisible=0;
+      glutPostRedisplay();
+   break;
+ case 2:if(movecarvar==0)
+  {
+  glutSpecialFunc(movecar);
+        movecarvar=1;
+     }
+ else{
+  glutSpecialFunc(inputKey);
+  movecarvar=0;
+     }
+  break;
+ }
+}
+void menu()
+{
+ int control;
+    int control1;
+
+
+
+ control= glutCreateMenu(ProcessMenu);
+ glutAddMenuEntry("**CONTROLS**",1);
+ glutAddMenuEntry("1)  UP KEY:untuk arah maju.",1);
+ glutAddMenuEntry("2)  DOWN KEY: untuk arah mundur.",1);
+ glutAddMenuEntry("3)  LEFT KEY: untuk belok kiri.",1);
+ glutAddMenuEntry("4)  RIGHT KEY:untuk belok kanan.",1);
+ glutAddMenuEntry("5)  d: pindah ke kanan. ",1);
+ glutAddMenuEntry("6)  a: pindah ke kiri.",1);
+ glutAddMenuEntry("7)  s: untuk arah menjauh.",1);
+ glutAddMenuEntry("8)  w:untuk arah mendekat.",1);
+ glutAddMenuEntry("9)  t: untuk arah dari atas.",1);
+ glutAddMenuEntry("10) q: keluar.",1);
+ glutAttachMenu(GLUT_RIGHT_BUTTON);
+    control1=glutCreateMenu(ProcessMenu1);
+
+ glutAddMenuEntry("MEMINDAHKAN MOBIL",2);
+ glutAttachMenu(GLUT_LEFT_BUTTON);
+
+}
+
+
+int main(int argc, char **argv)
+{
+ glutInit(&argc, argv);
+ glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
+ glutInitWindowPosition(0,0);
+ glutInitWindowSize(1010,710);
+ glutCreateWindow("parkir mobil");
+
+ initScene();
+
+ glutKeyboardFunc(processNormalKeys);
+ glutSpecialFunc(inputKey);
+    menu();
+ glutDisplayFunc(renderScene);
+ glutIdleFunc(renderScene);
+
+ glutReshapeFunc(changeSize);
+
+ glutMainLoop();
+
+ return(0);
+}
